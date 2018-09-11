66e240c502b2f578d6b89247030783da2c43f044
Atila Yumer <atila.yumer@hubparking.com>
2018-09-11 16:49:18 +0300

    Page rendering

--- src/main/java/io/vertx/quides/MainVerticle.java
+++ src/main/java/io/vertx/quides/MainVerticle.java
@@ -3,22 +3,27 @@ package io.vertx.quides;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.http.HttpServer;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.jdbc.JDBCClient;
 import io.vertx.ext.sql.SQLConnection;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.ext.web.templ.FreeMarkerTemplateEngine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.github.rjeschke.txtmark.Processor;
+
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class MainVerticle extends AbstractVerticle {
 
-  private static final String SQL_CREATE_PAGES_TABLE = "create table if not exists Pages (Id integer identity primary key, " +
-    "Name varchar(255) unique, Content clob)";
+  private static final String SQL_CREATE_PAGES_TABLE = "create table if not exists Pages (Id integer identity primary key, "
+    + "Name varchar(255) unique, Content clob)";
   private static final String SQL_GET_PAGE = "select Id, Content from Pages where Name = ?";
   private static final String SQL_CREATE_PAGE = "insert into Pages values (NULL, ?, ?)";
   private static final String SQL_SAVE_PAGE = "update Pages set Content = ? where Id = ?";
@@ -37,10 +42,8 @@ public class MainVerticle extends AbstractVerticle {
   private Future<Void> prepareDatabase() {
     Future<Void> future = Future.future();
 
-    dbClient = JDBCClient.createShared(vertx, new JsonObject()
-      .put("url", "jdbc:hsqldb:file:db/wiki")
-      .put("driver_class", "org.hsqldb.jdbcDriver")
-      .put("max_pool_size", 30));
+    dbClient = JDBCClient.createShared(vertx, new JsonObject().put("url", "jdbc:hsqldb:file:db/wiki")
+      .put("driver_class", "org.hsqldb.jdbcDriver").put("max_pool_size", 30));
 
     dbClient.getConnection(ar -> {
       if (ar.failed()) {
@@ -68,23 +71,21 @@ public class MainVerticle extends AbstractVerticle {
 
     Router router = Router.router(vertx);
     router.get("/").handler(this::indexHandler);
-//    router.get("/wiki/:page").handler(this::pageRenderingHandler);
-//    router.post().handler(BodyHandler.create());
-//    router.post("/save").handler(this::pageUpdateHandler);
-//    router.post("/create").handler(this::pageCreateHandler);
-//    router.post("/delete").handler(this::pageDeletionHandler);
+    router.get("/wiki/:page").handler(this::pageRenderingHandler);
+    router.post().handler(BodyHandler.create());
+    // router.post("/save").handler(this::pageUpdateHandler);
+    router.post("/create").handler(this::pageCreateHandler);
+    // router.post("/delete").handler(this::pageDeletionHandler);
 
-    server
-      .requestHandler(router::accept)
-      .listen(8080, ar -> {
-        if (ar.succeeded()) {
-          LOGGER.info("HTTP server running on port 8080");
-          future.complete();
-        } else {
-          LOGGER.error("Could not start a HTTP server", ar.cause());
-          future.fail(ar.cause());
-        }
-      });
+    server.requestHandler(router::accept).listen(8080, ar -> {
+      if (ar.succeeded()) {
+        LOGGER.info("HTTP server running on port 8080");
+        future.complete();
+      } else {
+        LOGGER.error("Could not start a HTTP server", ar.cause());
+        future.fail(ar.cause());
+      }
+    });
 
     return future;
   }
@@ -98,11 +99,7 @@ public class MainVerticle extends AbstractVerticle {
         connection.query(SQL_ALL_PAGES, res -> {
           connection.close();
           if (res.succeeded()) {
-            List<String> pages = res.result()
-              .getResults()
-              .stream()
-              .map(json -> json.getString(0))
-              .sorted()
+            List<String> pages = res.result().getResults().stream().map(json -> json.getString(0)).sorted()
               .collect(Collectors.toList());
             context.put("title", "Wiki home");
             context.put("pages", pages);
@@ -122,5 +119,60 @@ public class MainVerticle extends AbstractVerticle {
         context.fail(car.cause());
       }
     });
+  }
+
+  private static final String EMPTY_PAGE_MARKDOWN = "# A new page\n" + "\n" + "Feel-free to write in Markdown!\n";
+
+  private void pageRenderingHandler(RoutingContext context) {
+    String page = context.request().getParam("page");
+
+    dbClient.getConnection(car -> {
+      if (car.succeeded()) {
+
+        SQLConnection connection = car.result();
+        connection.queryWithParams(SQL_GET_PAGE, new JsonArray().add(page), fetch -> {
+          if (fetch.succeeded()) {
+
+            JsonArray row = fetch.result().getResults().stream().findFirst()
+              .orElseGet(() -> new JsonArray().add(-1).add(EMPTY_PAGE_MARKDOWN));
+            Integer id = row.getInteger(0);
+            String rawContent = row.getString(1);
+
+            context.put("title", page);
+            context.put("id", id);
+            context.put("newPage", fetch.result().getResults().size() == 0 ? "yes" : "no");
+            context.put("rawContent", rawContent);
+            context.put("content", Processor.process(rawContent));
+            context.put("timestamp", new Date().toString());
+
+            templateEngine.render(context, "templates", "/page.ftl", ar -> {
+              if (ar.succeeded()) {
+                context.response().putHeader("Content-Type", "text/html");
+                context.response().end(ar.result());
+              } else {
+                context.fail(ar.cause());
+              }
+            });
+          } else {
+            context.fail(fetch.cause());
+          }
+        });
+      } else {
+        context.fail(car.cause());
+      }
+    });
+  }
+
+  private void pageCreateHandler(RoutingContext context) {
+    String pageName = context.request().getParam("name");
+    String location = "/wiki/" + pageName;
+
+    if (pageName == null || pageName.isEmpty()) {
+      location = "/";
+    }
+
+    context.response().setStatusCode(303);
+    context.response().putHeader("Location", location);
+    context.response().end();
   }
 }
